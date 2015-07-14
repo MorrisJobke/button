d47fcaa85da509af745c83d3c1fbded3f3e27cbe
Morris Jobke <hey@morrisjobke.de>
2015-07-14 15:06:00 +0200

    simple app with button on login page

---
+++ appinfo/app.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * ownCloud - Button app
+ *
+ * This file is licensed under the Affero General Public License version 3 or
+ * later. See the COPYING file.
+ *
+ * @author Morris Jobke <hey@morrisjobke.de>
+ * @copyright Morris Jobke 2015
+ */
+
+
+\OC_App::registerLogIn(array('name' => 'Redirect to Google', 'href' => 'https://google.com'));
